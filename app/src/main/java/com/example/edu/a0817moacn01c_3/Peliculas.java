ff4e410f56dc37532dced08928c59dcb8037c14e
Unknown <pitrelliandres@gmail.com>
2017-10-17 20:26:07 -0300

    Agregue atributos a pelicua

--- app/src/main/java/com/example/edu/a0817moacn01c_3/Peliculas.java
+++ app/src/main/java/com/example/edu/a0817moacn01c_3/Peliculas.java
@@ -7,14 +7,20 @@ package com.example.edu.a0817moacn01c_3;
 public class Peliculas {
     private String nombre;
     private Integer imagen;
+    private Integer imagenPortada;
     private String genero;
     private String desc;
+    private Integer puntuacion;
+    private String aptoParaPublico;
 
-    public Peliculas(String nombre, Integer imagen, String genero, String desc) {
+    public Peliculas(String nombre, Integer imagen, String genero, String desc,Integer puntuacion,String aptoParaPublico,Integer imagenPortada) {
         this.nombre = nombre;
         this.imagen = imagen;
         this.genero = genero;
         this.desc = desc;
+        this.puntuacion=puntuacion;
+        this.aptoParaPublico=aptoParaPublico;
+        this.imagenPortada=imagenPortada;
     }
 
     public String getNombre() {
@@ -31,5 +37,17 @@ public class Peliculas {
 
     public String getDesc() {
         return desc;
+    }
+
+    public Integer getImagenPortada() {
+        return imagenPortada;
+    }
+
+    public Integer getPuntuacion() {
+        return puntuacion;
+    }
+
+    public String getAptoParaPublico() {
+        return aptoParaPublico;
     }
 }
