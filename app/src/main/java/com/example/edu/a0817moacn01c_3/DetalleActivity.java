2ecb5f4f0eb8220bf0e40261f76e21e602953a74
Unknown <babausias@gmail.com>
2017-10-19 03:07:51 -0300

    Pantalla Detalle

--- app/src/main/java/com/example/edu/a0817moacn01c_3/DetalleActivity.java
+++ app/src/main/java/com/example/edu/a0817moacn01c_3/DetalleActivity.java
@@ -1,13 +1,39 @@
 package com.example.edu.a0817moacn01c_3;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DetalleActivity extends AppCompatActivity {
+public class DetalleActivity extends AppCompatActivity{
+    private DetalleFragment detalleFragment;
+    private Bundle unBundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        detalleFragment = new DetalleFragment();
+
+        // Obtener info en del Bundle en el Intent emitido
+        recibirDatos();
+
+        detalleFragment.setArguments(unBundle);
+        fragmentTransaction.replace(R.id.contenedorDeFragmentsDetalle, detalleFragment);
+        fragmentTransaction.commit();
+
+
     }
+
+    public void recibirDatos(){
+        Intent unIntent = getIntent();
+        this.unBundle = unIntent.getExtras();
+        // Creo que masa adelante solo pasamos un identificador y el fragment hace la consulta en BD y/o API
+    }
+
 }
--- app/src/main/java/com/example/edu/a0817moacn01c_3/DetalleFragment.java
+++ app/src/main/java/com/example/edu/a0817moacn01c_3/DetalleFragment.java
@@ -1,17 +1,28 @@
 package com.example.edu.a0817moacn01c_3;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetalleFragment extends Fragment {
+    private Peliculas pelicula;
+    private ImageView imagen;
+    private ImageView imagenPortada;
+    private TextView puntuacion;
+    private TextView ano;
+    private TextView genero;
+    private TextView clasificacion;
+    private TextView sinopsis;
 
 
     public DetalleFragment() {
@@ -22,8 +33,35 @@ public class DetalleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Bundle unBundle = getArguments();
+        pelicula = new Peliculas(unBundle.getString("nombre"),unBundle.getInt("imagen"),unBundle.getInt("imagenPortada"),unBundle.getString("genero"),unBundle.getString("desc"),unBundle.getInt("puntuacion"),unBundle.getString("aptoTodoPublico"));
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalle, container, false);
+        View view = inflater.inflate(R.layout.fragment_detalle, container, false);
+        this.imagen = view.findViewById(R.id.imageView_ImagenPelicula);
+        this.imagenPortada = view.findViewById(R.id.imageview_detalleBackdrop);
+        this.puntuacion = view.findViewById(R.id.textview_detallePuntuacion);
+        //this.ano =
+        this.genero = view.findViewById(R.id.textview_genero);
+        this.clasificacion = view.findViewById(R.id.textview_clasificacion);
+        this.sinopsis = view.findViewById(R.id.textview_descripcion);
+
+        mostrarInformacion();
+
+        return view;
+
+
+    }
+
+    public void mostrarInformacion(){
+        getActivity().setTitle(pelicula.getNombre());
+        imagen.setImageResource(pelicula.getImagen());
+        imagenPortada.setImageResource(pelicula.getImagenPortada());
+        //puntuacion.setText(pelicula.getPuntuacion());
+        //ano.setText(pelicula.getAno());
+        genero.setText(pelicula.getGenero());
+        clasificacion.setText(pelicula.getAptoParaPublico());
+        sinopsis.setText(pelicula.getDesc());
+
     }
 
 }
