45860c9940f26c54aba5b646ecc985923c0fb818
Unknown <pitrelliandres@gmail.com>
2017-11-24 20:33:31 -0300

    Mas Cambios

--- app/src/main/java/com/example/edu/a0817moacn01c_3/View/ActivityOnBoarding.java
+++ app/src/main/java/com/example/edu/a0817moacn01c_3/View/ActivityOnBoarding.java
@@ -1,9 +1,11 @@
 package com.example.edu.a0817moacn01c_3.View;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.edu.a0817moacn01c_3.R;
 
@@ -21,5 +23,10 @@ ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
         AdapterViewPagerOnBoarding adapterViewPagerOnBoarding= new AdapterViewPagerOnBoarding(fragmentManager);
         viewPager.setAdapter(adapterViewPagerOnBoarding);
 
+    }
+    public void irAPantallaPrincipal(View view){
+        Intent intent= new Intent(this,MainActivity.class);
+        startActivity(intent);
+        
     }
 }
