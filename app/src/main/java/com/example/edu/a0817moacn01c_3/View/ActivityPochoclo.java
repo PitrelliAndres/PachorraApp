d3b500d0275729d80cabc894ec644f3c6340dcbc
Unknown <pitrelliandres@gmail.com>
2017-12-12 18:41:46 -0300

    Firebase0.1

---
+++ app/src/main/java/com/example/edu/a0817moacn01c_3/View/ActivityPochoclo.java
@@ -0,0 +1,15 @@
+package com.example.edu.a0817moacn01c_3.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.edu.a0817moacn01c_3.R;
+
+public class ActivityPochoclo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pochoclo);
+    }
+}
---
+++ app/src/main/java/com/example/edu/a0817moacn01c_3/View/PochocloFragment.java
@@ -0,0 +1,30 @@
+package com.example.edu.a0817moacn01c_3.View;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.edu.a0817moacn01c_3.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PochocloFragment extends Fragment {
+
+
+    public PochocloFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pochoclo, container, false);
+    }
+
+}
